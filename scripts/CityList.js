187bff80599ddd6369ac9ae5e3c8314f2424ef16
Ryan Allred <ryanallred91@gmail.com>
2022-10-07 10:07:38 -0500

    pushing new stuff

--- scripts/CityList.js
+++ scripts/CityList.js
@@ -1,13 +1,16 @@
-import { getWalkers } from "./database.js"
+import { Assignments } from "./Assignments.js"
+import { getWalkers, getCities, getWalkerCities } from "./database.js"
 
 const walkers = getWalkers()
+const cities = getCities()
+const walkerCities = getWalkerCities()
 
 
 export const CityList = () => {
     let citiesHTML = "<ol>"
 
-    for (const walker of walkers) {
-        citiesHTML += `<li>${walker.city}</li>`
+    for (const city of cities) {
+        citiesHTML += `<li>${city.name}</li>`
     }
 
     citiesHTML += "</ol>"
@@ -15,3 +18,50 @@ export const CityList = () => {
     return citiesHTML
 }
 
+// const walkerCitiesForWalker = (walker) => {
+//     assignments = []
+//     for (const assignment of walkerCities) {
+//         if (assignment.walkerId === walker.id) {
+//             assignments.push(assignment)
+//         }
+//     }
+//     return assignments
+// }
+
+// const assignedCityNames = (assignments) => {
+//     let cityNames = ""
+//     for (const assignment of assignments) {
+
+//         for (const city of cities) {
+//             if (city.id === assignment.cityId) {
+//                 cityNames = `${cityNames} and ${city.name}`
+//             }
+//         }
+//     }
+//     return cityNames
+// }
+
+// for (const walker of walkers) {
+//     if (walker.id === parseInt(walkerId)) {
+//         const assignments = walkerCitiesForWalker(walker)
+//         const cities = assignedCityNames(assignments)
+
+//         window.alert(`${walker.name} services ${cities}`)
+//     }
+// }
+
+
+
+
+
+// export const CityList = () => {
+//     let citiesHTML = "<ol>"
+
+//     for (const walker of walkers) {
+//         citiesHTML += `<li>${walker.city}</li>`
+//     }
+
+//     citiesHTML += "</ol>"
+
+//     return citiesHTML
+// }
